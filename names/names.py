5b8ef34e896f385ffaa184690e0fa1b0777c69a3
kaverndsp <kaverndsp@users.noreply.github.com>
2020-06-12 12:33:00 -0500

    mvp I believe. attempting to refactor ring buffer

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import BSTNode
 
 start_time = time.time()
 
@@ -13,14 +14,19 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+new_data = BSTNode("Jean")
+
+for i in names_1:
+    new_data.insert(i)
+for i in names_2:
+    if new_data.contains(i):
+        duplicates.append(i)
+
+# O(n^2) -> O(n) & look up is O(log(n))
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,35 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.queue = []
+        self.queue_tail = None
 
     def append(self, item):
-        pass
+        if len(self.queue) < self.capacity:
+            # if this is the first item to be added,
+            # set oldest to this item if it's the first
+            if len(self.queue) == 0:
+                self.queue_tail = 0
+            else:
+                self.queue_tail += 1
+            # append the item
+            self.queue.append(item)
+
+        # what if the queue is AT capacity?
+        else:
+            # update the oldest index
+
+            # if the index was at the last item in
+            # the array, move index to the front
+            if self.queue_tail == self.capacity-1:
+                self.queue_tail = 0
+            # otherwise, just increment the oldest
+            # index to the next item to the right
+            else:
+                self.queue_tail += 1
+
+            # replace item at oldest index
+            self.queue[self.queue_tail] = item
 
     def get(self):
-        pass
+        return self.queue

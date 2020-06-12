e510b4e1436ada67494c0991981686bf702a3cf8
kaverndsp <kaverndsp@users.noreply.github.com>
2020-06-12 12:54:33 -0500

    trying to think of how to implement dll in ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,29 +6,20 @@ class RingBuffer:
 
     def append(self, item):
         if len(self.queue) < self.capacity:
-            # if this is the first item to be added,
-            # set oldest to this item if it's the first
+
             if len(self.queue) == 0:
                 self.queue_tail = 0
             else:
                 self.queue_tail += 1
-            # append the item
+
             self.queue.append(item)
 
-        # what if the queue is AT capacity?
         else:
-            # update the oldest index
-
-            # if the index was at the last item in
-            # the array, move index to the front
             if self.queue_tail == self.capacity-1:
                 self.queue_tail = 0
-            # otherwise, just increment the oldest
-            # index to the next item to the right
             else:
                 self.queue_tail += 1
 
-            # replace item at oldest index
             self.queue[self.queue_tail] = item
 
     def get(self):
